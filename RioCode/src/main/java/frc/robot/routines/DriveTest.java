65578c03386cbb518ec5ac645fc37c9f032880e6
coalman321 <coalman321@users.noreply.github.com>
2018-12-10 16:39:24 -0500

    tweaked path

--- RioCode/src/main/java/frc/robot/routines/DriveTest.java
+++ RioCode/src/main/java/frc/robot/routines/DriveTest.java
@@ -5,6 +5,7 @@ import frc.lib.AutoTrajectory.Translation2d;
 import frc.lib.statemachine.StateMachineDescriptor;
 import frc.robot.actions.FollowPath;
 import frc.robot.actions.PathCompletionWait;
+import frc.robot.actions.Wait;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,19 +13,28 @@ import java.util.List;
 public class DriveTest extends StateMachineDescriptor {
 
     public DriveTest() {
+
+        double speedLimit = 10;
         //create trajectory
         List<Path.Waypoint> traj = new ArrayList<>();
-        traj.add(new Path.Waypoint(new Translation2d(0, 0), 10));
-        traj.add(new Path.Waypoint(new Translation2d(120, 0), 10)); // +x is forward and +y is right
+        traj.add(new Path.Waypoint(new Translation2d(0, 0), speedLimit));
+        traj.add(new Path.Waypoint(new Translation2d(30, 0), speedLimit));
+        traj.add(new Path.Waypoint(new Translation2d(60,-20), speedLimit));
+        traj.add(new Path.Waypoint(new Translation2d(90, 0), speedLimit));
+        traj.add(new Path.Waypoint(new Translation2d(120, 0), speedLimit)); // +x is forward and +y is left
 
         List<Path.Waypoint> rev = new ArrayList<>();
-        rev.add(new Path.Waypoint(new Translation2d(120, 0), 10));
-        rev.add(new Path.Waypoint(new Translation2d(0, 0), 10)); // +x is forward and +y is right
+        rev.add(new Path.Waypoint(new Translation2d(120, 0), speedLimit));
+        rev.add(new Path.Waypoint(new Translation2d(90, 0), speedLimit));
+        rev.add(new Path.Waypoint(new Translation2d(60,-20), speedLimit));
+        rev.add(new Path.Waypoint(new Translation2d(30, 0), speedLimit));
+        rev.add(new Path.Waypoint(new Translation2d(0, 0), speedLimit)); // +x is forward and +y is left
 
         //create state descriptor
-        addSequential(new FollowPath(new Path(traj), false), 10000); // 10 seconds
+        addSequential(new FollowPath(new Path(traj), false), 15000); // 10 seconds
         addSequential(new PathCompletionWait(), 10000);
-        addSequential(new FollowPath(new Path(rev), true), 10000); // 10 seconds
+        addSequential(new Wait(1.0), 1000);
+        addSequential(new FollowPath(new Path(rev), true), 15000); // 10 seconds
     }
 
 }
